2b85c807f2243790b7583e3a90da7da6b3259030
Manuel Vázquez <vazquez.manuel.ds@gmail.com>
2026-06-29 07:44:03 +0200

    Chat endpoint implementation, languages and icons

--- main.py
+++ main.py
@@ -7,12 +7,14 @@
 
 from pathlib import Path
 from fastapi import FastAPI, Request
+# from fastapi.params import Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 
 # ca: per llegir els fitxers de traducció
 import json
+import time
 
 templates = Jinja2Templates(
     directory="templates"
@@ -34,28 +36,70 @@ default_language = "en"
 current_language = default_language
 # ca: Carreguem les traduccions
 translations = locales.get(current_language, locales[default_language])
-print(f"Translations: {translations}")
+# ca: posteriorment es dupliquen al mirar la cookie
+# print(f"Translations: {translations}")
 
-privateAIH = FastAPI(title="Private AI Hub", version="0.1.1")
+privateAIH = FastAPI(title="Private AI Hub", version="0.2.0")
 
 # ca: Carreguem directori static a FastAPI
 privateAIH.mount("/static", StaticFiles(directory="static"), name="static")
 
+def mira_cookie_idioma(request: Request) -> str:
+    # ca: Intentem obtenir l'idioma de la cookie
+    lang = request.cookies.get("posaAquestIdioma")
+    # ca: Si no hi ha cookie, utilitzem l'idioma per defecte
+    # print(f"Idioma de la cookie: {lang}")
+    if not lang:
+        lang = default_language
+    return lang
+
 # @privateAIH.get("/")
 @privateAIH.get("/",response_class=HTMLResponse)
+# @privateAIH.get("/",response_class=HTMLResponse, dependencies=[Depends(mira_cookie_idioma)])
 # de moment sense async def
 # TODO: Implementar async def quan sigui necessari
 async def privateAIH_root(request: Request):
+    global current_language
+    global translations
+    current_language = mira_cookie_idioma(request)
+    translations = locales.get(current_language, locales[default_language])
     les_variables = {
         "request": request,
-        "byAuthor": translations.get("byAuthor", "by"),
+        "byAuthor": translations.get("byAuthor", "by Manuel Vázquez"),
         "msgPlaceholder": translations.get("msgPlaceholder", "Write your message..."),
-        "sendButton": translations.get("sendButton", "Send")
+        "sendButton": translations.get("sendButton", "Send"),
+        # afegim una versió per forçar la recàrrega dels fitxers estàtics
+        "version": int(time.time()),
+        "current_language": current_language
     }
     # return templates.TemplateResponse("index.html", {"request": request})
     # return templates.TemplateResponse("index.html", {"request": {}})
-    print(f"Variables: {les_variables}")
+    # print(f"Variables: {les_variables}")
     return templates.TemplateResponse(request,"index.html", les_variables)
+
+@privateAIH.post("/posa_idioma", response_class=JSONResponse)
+async def posa_idioma(request: Request):
+    global current_language
+    global translations
+    # ca: llegim el body de la petició
+    body = await request.json()
+    # ca: actualitzem l'idioma
+    current_language = body.get("language", default_language)
+    # ca: carreguem les traduccions
+    translations = locales.get(current_language, locales[default_language])
+    response = JSONResponse(translations)
+    response.set_cookie(key="posaAquestIdioma", value=current_language, max_age=2592000)
+    # print(f"Response: {response}")
+    return response
+
+@privateAIH.post("/chat", response_class=JSONResponse)
+async def chat(request: Request):
+    # ca: llegim el body de la petició
+    body = await request.json()
+    # print(f"Body: {body}")
+    # ca: retornem la resposta
+    return JSONResponse({"response": f"Sent / Enviado / Enviat:\n\n{body['message']}"})
+
 
 # arranquem la API
 if __name__ == "__main__":
@@ -64,4 +108,3 @@ if __name__ == "__main__":
     # uvicorn.run(privateAIH, host="0.0.0.0", port=8000)
     # ca: Aquest el faig amb --reload per desenvolupar
     uvicorn.run("main:privateAIH", host="0.0.0.0", port=8000, reload=True)
-
--- static/app.js
+++ static/app.js
@@ -2,27 +2,171 @@
 // ja hem fet les proves del scroll
 // he canviat bloqueCentral.innerHTML per
 // bloqueCentral.textContent segons chatgpt
-const textDeProva = "...esperant un missatge";
+const textDeProva = "...waiting a message / esperando un mensaje / esperant un missatge";
 const mensajeEnvido = "<div style='padding: 5px 0px;'><b>Missatge enviat:</b><br></div>";
-let bloqueCentral = document.getElementById("bloque-central");
-bloqueCentral.textContent = textDeProva;
+const bloqueCentral = document.getElementById("bloque-central");
+// canviem aquestes 2 linies ja que el construim directament
+// let bloqueCentral = document.getElementById("bloque-central");
+//bloqueCentral.textContent = textDeProva;
 const mensaje = document.getElementById("el-mensaje");
 const bloqueFormulario = document.getElementById("bloque-formulario");
 const botonEnviar = document.getElementById("enviar");
+const idiomaSelect = document.getElementById("idioma");
 mensaje.addEventListener('input', () => {
   // Si el texto limpio es igual a "", 'disabled' será true.
   botonEnviar.disabled = mensaje.value.trim() === '';
 });
+function afegirMissatge(text, tipus) {
+  // console.log("Afegir missatge");
+  const node = document.createElement("div");
+  node.classList.add(tipus)
+  node.textContent = text;
+  bloqueCentral.appendChild(node);
+  return node;
+}
+function afegirMsgPrompt(text) {
+  return afegirMissatge(text, "msgprompt");
+}
+function afegirMsgResponse(text) {
+  return afegirMissatge(text, "msgresponse");
+}
+function crearMsgStatus() {
+  const node = document.createElement("div");
+  node.classList.add("msgstatus");
+  node.textContent = "Thinking... / Pensando... / Pensant...";
+  bloqueCentral.appendChild(node);
+  return {node: node,
+    inici: performance.now()
+  };
+}
+function finalitzarMsgStatus(status) {
+  const temps = ((performance.now() - status.inici)/1000).toFixed(2);
+  status.node.classList.add("msgstatusfinal");
+  status.node.classList.remove("msgstatus");
+  status.node.textContent = `Thought... / Pensado... / Pensat... (${temps}s.)`;
+}
 
-bloqueFormulario.addEventListener('submit', (e) => {
+bloqueFormulario.addEventListener('submit', async(e) => {
+  // console.log("Submit handler called"); // Add this
   e.preventDefault();
-  // console.log(mensaje.value);
-  if (bloqueCentral.textContent === textDeProva) {
-    bloqueCentral.textContent = mensajeEnvido + mensaje.value;
-  } else {
-    bloqueCentral.textContent += "<br><br>" + mensajeEnvido + mensaje.value;
+  // // console.log(mensaje.value);
+  document.querySelector("#mensaje-inicial")?.remove();
+
+  afegirMsgPrompt(mensaje.value.trim());
+  const estat = crearMsgStatus();
+  // el replicarem cada cop que afegim un o més missatges seguits
+  bloqueCentral.scrollTop = bloqueCentral.scrollHeight;
+  try {
+   const resposta = await fetch("/chat", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+     },
+     body: JSON.stringify({
+       message: mensaje.value.trim(),
+     }),
+   });
+   const data = await resposta.json();
+   afegirMsgResponse(data.response);
+   finalitzarMsgStatus(estat);
+  } catch (error) {
+    console.error(error);
+    estat.node.classList.add("msgstatusfinal");
+    estat.node.classList.remove("msgstatus");
+    estat.node.textContent = `Error: ${error.message}`;
   }
+
   bloqueCentral.scrollTop = bloqueCentral.scrollHeight;
   mensaje.value = "";
+  mensaje.style.height = "auto";
+  botonEnviar.disabled = true;
+
   mensaje.focus();
+});
+
+
+// bloqueFormulario.addEventListener('submit', (e) => {
+//   console.log("Submit handler called"); // Add this
+//   e.preventDefault();
+//   // console.log(mensaje.value);
+//   if (bloqueCentral.textContent === textDeProva) {
+//     bloqueCentral.textContent = mensajeEnvido + mensaje.value;
+//   // } else {
+//   //   bloqueCentral.textContent += "<br><br>" + mensajeEnvido + mensaje.value;
+//   // }
+//   } else {
+//     bloqueCentral.innerHTML += "<br><br>" + mensajeEnvido + mensaje.value;
+//   }
+//   bloqueCentral.scrollTop = bloqueCentral.scrollHeight;
+//   mensaje.value = "";
+//   mensaje.style.height = "auto";
+//   botonEnviar.disabled = true;
+//   mensaje.focus();
+// });
+
+//ca: per capturar l'idioma
+async function reclamaIdioma(quinIdioma) {
+  // console.log("Idioma seleccionat: " + quinIdioma);
+  // ca: fem una petició POST per actualitzar l'idioma
+  const response = await fetch("/posa_idioma", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({language: quinIdioma}),
+  });
+ const nou_idioma = await response.json();
+ // console.log(nou_idioma);
+ canvia_idioma(quinIdioma, nou_idioma);
+}
+
+// idiomaSelect.addEventListener('change', async () => {
+//   console.log("Idioma seleccionat: " + idiomaSelect.value);
+//   // ca: fem una petició POST per actualitzar l'idioma
+//   const response = await fetch("/posa_idioma", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({language: idiomaSelect.value}),
+//   });
+//  const nou_idioma = await response.json();
+//  console.log(nou_idioma);
+//  canvia_idioma(idiomaSelect.value, nou_idioma);
+// });
+idiomaSelect.addEventListener('change', async (elEvent) => {
+  // console.log("Idioma seleccionat: " + elEvent.target.value);
+  await reclamaIdioma(elEvent.target.value);
+});
+
+function canvia_idioma(amb_idioma, textos) {
+  // ca: canviar els textos segons l'idioma
+  // console.log("Canviant idioma a: " + amb_idioma);
+  // console.log("textos: " + JSON.stringify(textos));
+        document.querySelectorAll('[idio]').forEach(elemento => {
+            // console.log(elemento.getAttribute('idio'));
+            const cada = elemento.getAttribute('idio');
+            elemento.textContent = textos[cada];
+        });
+
+        document.querySelectorAll('[idio-place]').forEach(elemento => {
+            // console.log(elemento.getAttribute('idio-place'));
+            const cada = elemento.getAttribute('idio-place');
+            elemento.setAttribute("placeholder",textos[cada]);
+        });
+        document.documentElement.lang = amb_idioma;
+};
+
+mensaje.addEventListener("input", () => {
+    mensaje.style.height = "auto";
+    mensaje.style.height = mensaje.scrollHeight + "px";
+    botonEnviar.disabled = mensaje.value.trim() === '';
+
+});
+mensaje.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && e.ctrlKey) {
+        e.preventDefault();
+        //botonEnviar.click();
+        bloqueFormulario.requestSubmit();
+    }
 });
